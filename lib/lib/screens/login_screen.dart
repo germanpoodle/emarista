af05597776b333759969f38049aed9a57f8c6c6d
shuakyle <93431935+germanpoodle@users.noreply.github.com>
2024-05-27 12:59:18 +0800

    add splash screen

--- lib/lib/screens/login_screen.dart
+++ lib/lib/screens/login_screen.dart
@@ -3,19 +3,20 @@ import 'dart:ui';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:login/lib/screens/homepage.dart';
+import 'package:emarista_app/create_account_screen.dart';
+import 'package:emarista_app/main.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends State<ToLoginPage> {
   final _formKey = GlobalKey<FormState>();
   String? studID;
   String? studName;
 
-  LoginPage({super.key, this.studID, this.studName});
+  LoginPage({this.studID, this.studName});
 
   Future<bool> _login(String studentID, String studentName) async {
     try {
       var url = Uri.parse(
-          'http://192.168.100.140/localconnect/login.php'); // Replace with your PHP server URL
+          'http://localhost/localconnect/local.php'); // Replace with your PHP server URL
       var response = await http
           .post(url, body: {'studID': studentID, 'studName': studentName});
 
@@ -41,150 +42,189 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("E-MARISTA"),
-        automaticallyImplyLeading: true,
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("background.jpg"),
-            fit: BoxFit.cover,
-          ),
+        appBar: AppBar(
+          title: const Text("Login"),
+          automaticallyImplyLeading: true,
         ),
-        child: Center(
-          child: SingleChildScrollView(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.yellow,
-                          width: 2,
+        body: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("/Users/gabz_1/Desktop/eMARISTA/emarista_app/assets/background.jpg"),
+            fit: BoxFit.cover,
+          )),
+          child: Center(
+            child: SingleChildScrollView(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.yellow,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(5),
                         ),
-                        borderRadius: BorderRadius.circular(5),
+                        padding: const EdgeInsets.all(8),
+                        child: const Text(
+                          'WELCOME BACK MARISTA',
+                          style: TextStyle(
+                            fontSize: 64,
+                            color: Color.fromARGB(255, 55, 240, 86),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                      padding: const EdgeInsets.all(8),
-                      child: const Text(
-                        'WELCOME BACK MARISTA',
+                      const SizedBox(height: 16),
+                      const Text(
+                        'SIGN IN TO CONTINUE.',
+                        textAlign: TextAlign.center,
                         style: TextStyle(
-                          fontSize: 64,
-                          color: Color.fromARGB(255, 55, 240, 86),
-                          fontWeight: FontWeight.bold,
+                          fontSize: 32,
+                          color: Color.fromARGB(255, 62, 245, 69),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                    const Text(
-                      'SIGN IN TO CONTINUE.',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 32,
-                        color: Color.fromARGB(255, 62, 245, 69),
-                      ),
-                    ),
-                    const SizedBox(height: 32),
-                    Form(
-                      key: _formKey,
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly,
-                              LengthLimitingTextInputFormatter(13),
-                            ],
-                            keyboardType: TextInputType.number,
-                            decoration: const InputDecoration(
-                              labelText: 'ID Number',
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.black),
+                      const SizedBox(height: 32),
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly,
+                                LengthLimitingTextInputFormatter(13),
+                              ],
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                labelText: 'ID Number',
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black),
+                                ),
                               ),
-                              filled: true,
-                              fillColor: Colors.white,
-                              labelStyle: TextStyle(color: Colors.black),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your ID number';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) => studID = value,
                             ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter your ID number';
-                              }
-                              return null;
-                            },
-                            onSaved: (value) => studID = value,
-                          ),
-                          const SizedBox(height: 16),
-                          TextFormField(
-                            obscureText: true,
-                            decoration: const InputDecoration(
-                              labelText: 'Password',
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.black),
+                            const SizedBox(height: 16),
+                            TextFormField(
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                labelText: 'Password',
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(),
                               ),
-                              filled: true,
-                              fillColor: Colors.white,
-                              labelStyle: TextStyle(color: Colors.black),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your password';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) => studName = value,
                             ),
-                            onSaved: (value) => studName = value,
-                          ),
-                          const SizedBox(height: 16),
-                          ElevatedButton(
-                            onPressed: () async {
-                              if (_formKey.currentState!.validate()) {
-                                _formKey.currentState!.save();
-                                bool isLoggedIn =
-                                    await _login(studID!, studName!);
-                                if (isLoggedIn) {
-                                  Navigator.pushReplacement(
+                            const SizedBox(height: 16),
+                            ElevatedButton(
+                              onPressed: () async {
+                                if (_formKey.currentState!.validate()) {
+                                  _formKey.currentState!.save();
+                                  bool isLoggedIn =
+                                      await _login(studID!, studName!);
+                                  if (isLoggedIn) {
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => MyApp()),
+                                    );
+                                  } else {
+                                    // Handle login failure
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: const Text('Login Failed'),
+                                          content: const Text(
+                                              'Failed to log in. Please check your credentials and try again.'),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: const Text('OK'),
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  }
+                                }
+                              },
+                              child: const Text('Login'),
+                            ),
+                            const SizedBox(height: 16),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: ElevatedButton(
+                                style: TextButton.styleFrom(
+                                  foregroundColor: Colors.black,
+                                  side: const BorderSide(color: Colors.black),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    side: const BorderSide(
+                                        color: Colors.black, width: 1),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 16, vertical: 8),
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
                                     context,
                                     MaterialPageRoute(
-                                        builder: (context) => Homepage()),
+                                      builder: (context) =>
+                                          const CreateAccount(),
+                                    ),
                                   );
-                                } else {
-                                  // Handle login failure
-                                  showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title: const Text('Login Failed'),
-                                        content: const Text(
-                                            'Failed to log in. Please check your credentials and try again.'),
-                                        actions: [
-                                          TextButton(
-                                            onPressed: () {
-                                              Navigator.pop(context);
-                                            },
-                                            child: const Text('OK'),
-                                          ),
-                                        ],
-                                      );
-                                    },
-                                  );
-                                }
-                              }
-                            },
-                            child: const Text('Login'),
-                          ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: TextButton(
-                              onPressed: () {},
-                              child: const Text('Forgot password?'),
+                                },
+                                child: const Text('Create Account'),
+                              ),
                             ),
-                          ),
-                        ],
+                            const SizedBox(height: 16),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: ElevatedButton(
+                                style: TextButton.styleFrom(
+                                  foregroundColor: Colors.black,
+                                  side: const BorderSide(color: Colors.black),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    side: const BorderSide(
+                                        color: Colors.black, width: 1),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 16, vertical: 8),
+                                ),
+                                onPressed: () {},
+                                child: const Text('Forgot password?'),
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,96 @@
 import 'package:flutter/material.dart';
-import 'lib/screens/login_screen.dart';
-
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:lottie/lottie.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'login_screen.dart'; // Ensure this file is correctly imported
 
 void main() {
-  runApp(const LoginAccount());
+  runApp(const MyApp());
 }
 
-class LoginAccount extends StatelessWidget {
-  const LoginAccount({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "test",
+      title: "E-MARISTA",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            child: Center(
+              child: Lottie.network(
+                  'https://lottie.host/4b5a3166-80b9-4398-8053-ff2e26c8ee81/KKPhnT9m6D.json'),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Welcome to E-Marista',
+            style: GoogleFonts.caveat(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Your Local Book Room Supplies in a Screen",
+            style: GoogleFonts.robotoSlab(
+              fontSize: 18,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) => LoginPage(),
+                  transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                    const begin = 0.0;
+                    const end = 1.0;
+                    const curve = Curves.easeInOut;
+
+                    var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                    var fadeAnimation = animation.drive(tween);
+
+                    return FadeTransition(
+                      opacity: fadeAnimation,
+                      child: child,
+                    );
+                  },
+                ),
+              );
+            },
+            child: Text("Let's get started"),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.blue,
+              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+              textStyle: TextStyle(fontSize: 20),
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
     );
   }
 }
